0c9443f73743be2c17073d9328eae7d85927e8a1
LukasMueIIer <Mueller.Lukas@outlook.com>
2024-02-01 00:49:41 +0100

    It can now take the screenshots now we have to add automatically taking one dont fish screenshot and save them in the propper directories

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 # YOUR GAME MUST BE IN WINDOWED MODE
 otherwise we cant take a screenshot (easily)
 
+# place your crosshair so it aims 4 blocks infront of you, align with blocks
+
 ## Requirements so far
 Tensoflow
 pyautogui
 OpenCV 
 pywin32
+pydirectinput
--- captureData.py
+++ captureData.py
@@ -4,18 +4,72 @@ import numpy as np
 import pyautogui
 import imutils
 import cv2
-print("TABBB")
-time.sleep(5)
-image = pyautogui.screenshot()
-image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-x = image.shape[0]
-y = image.shape[1]
-width = 0.05 #width percentage of the picture we wana use
-height = 0.4 #height percentage of the picture we wana use
-dx = round(x * height / 2)
-dy = round(y * width / 2)
-xm = round(0.5 * x)
-ym = round(0.5 * y)
-image = image[(xm - dx):(xm + dx),(ym - dy):(ym + dy),:]
-cv2.imwrite("in_memory_to_disk.png", image)
+from datetime import datetime
+import pydirectinput
+import win32api
+import win32con
+
+#config
+checkClickFrequ = 500 #frequenzy at which we check button clicks
+frequency = 4 #frequenzy in Hz at which NN will run -> we add a random delay to taking the screenshot
+delayFishing = 4 #re throw rod after ... seconds
+
+print("Tab into Minecraft, make sure its windowed on full size")
+print("Aim reel 4 blocks away")
+
+
+def savePicture():
+    image = pyautogui.screenshot()
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    x = image.shape[0]
+    y = image.shape[1]
+    width = 0.05 #width percentage of the picture we wana use
+    heightUp = 0.08 #height percentage of the picture we wana use upwards of cursor
+    heightDown = 0.03 #height percentage below cursor
+    dxu = round(x * heightUp)
+    dxl = round(x * heightDown)
+    dy = round(y * width / 2)
+    xm = round(0.5 * x)
+    ym = round(0.5 * y)
+    image = image[(xm - dxu):(xm + dxl),(ym - dy):(ym + dy),:]
+    filename = f'{datetime.now():%Y_%m_%d_%H_%M_%S%z}'
+    filename =  filename + ".png"
+    cv2.imwrite(filename, image)
+
+run = True
+ds = 1/checkClickFrequ 
+reeled = False
+t = time.time()
+while(run):
+    if(not reeled):
+        #throw reel
+        time.sleep(delayFishing)
+        pydirectinput.click(button='right')
+        print("Threw Reel")
+        reeled = True
+    else:
+        #check for button presses
+        state_right = win32api.GetKeyState(0x02)
+        state_left = win32api.GetKeyState(0x01)
+        if state_right < 0:
+            print("reeled in .. taking picture")
+            reeled = False
+            savePicture()
+        elif(state_left < 0):
+            run = False
+            print("Terminating ...")
+        #achieve target frequenzy
+        time.sleep(ds)
+
+
+print("Well done")
+    
+
+
+
+
+
+
+
+
 
